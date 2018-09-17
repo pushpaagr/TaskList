ade133b04d45200dd92c88d48af2af60df2496f3
Pushpa Agr <pushpaagr108@gmail.com>
2018-09-17 16:29:46 -0700

    added show method and some colors

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,4 +11,12 @@ class TasksController < ApplicationController
   def index
     @tasks = TASKS
   end
+
+  def show
+    task_id = params[:id].to_i
+    @task = TASKS[task_id]
+    if @task.nil?
+      head :not_found
+    end
+  end
 end
