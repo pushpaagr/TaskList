ef4c38b0e8cc78c11696ee67b9c5ac9953d8e87b
Pushpa Agr <pushpaagr108@gmail.com>
2018-09-17 13:56:45 -0700

    added Tasks constant variable

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,14 @@
+TASKS = [
+  { task: "one", status:  "done"},
+  {task: "two", status: "not done"},
+  {task: "three", status: "done"}
+]
+
+
+
 class TasksController < ApplicationController
+
+  def index
+    @tasks = TASKS
+  end
 end
